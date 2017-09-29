a79dc7391fe54a1848986745d70b9cd321e8d7ef
zzk <zhekunz2@illinois.edu>
2017-09-29 02:51:05 -0500

    node

--- MazeAI/Node.java
+++ MazeAI/Node.java
@@ -2,50 +2,55 @@
 
 class Node{
 
-	public int x;
-	public int y;
-	public Node up_child;
-	public Node down_child;
-	public Node left_child;
-	public Node right_child;
+    public int x;
+    public int y;
+    public int gScore;
+    public Node up_child;
+    public Node down_child;
+    public Node left_child;
+    public Node right_child;
+    public Node parent;
 
-	//has visited;
-	public Boolean isVisited;
-  //three way blocked
-	public Boolean isBlocked;
+    //has visited;
+    public Boolean isVisited;
+    //three way blocked
+    public Boolean isBlocked;
 
-  public char value;//store for curr node's value;
+    public char value;//store for curr node's value;
 
-	//distance from goal state
-	public int curr_distance;
-  // arraylist of all children;
-  public List<Node> neighbor;
-
-
-
-  public int manhattanDistance(Node dest) {
-      int dist = Math.abs(this.x - dest.x) + Math.abs(this.y - dest.y);
-      return dist;
-  }
+    //distance from goal state
+    public int curr_distance;
 
 
-  public Node(int x, int y, char value){
-    this.x = x;
-    this.y = y;
 
-    this.value = value;
+    public int manhattanDistance(Node dest) {
+        int dist = Math.abs(this.x - dest.x) + Math.abs(this.y - dest.y);
+        return dist;
+    }
 
-    up_child = null;
-    down_child = null;
-    left_child = null;
-    right_child = null;
+    public boolean equals (Node other){
+        return ((this.x == other.x) && (this.y == other.y));
+    }
+    public int gScore(){
+        if (this.parent == null){
+            return 0;
+        }
+        return 1+this.parent.gScore();
+    }
+    public Node(int x, int y, char value){
+        this.x = x;
+        this.y = y;
 
-    isVisited = false;
+        this.value = value;
 
-    isBlocked = false;
+        up_child = null;
+        down_child = null;
+        left_child = null;
+        right_child = null;
 
-    neighbor = new ArrayList<Node>;
+        isVisited = false;
 
-  }
-  
+        isBlocked = false;
+    }
+
 }
