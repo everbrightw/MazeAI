0b2aa7c1971eb11ea143d90c95dbb7aedb3dac73
everbrightw <yusencs@163.com>
2017-09-29 03:21:55 -0500

    a star to be fucking debug

--- MazeAI/AStar.java
+++ MazeAI/AStar.java
@@ -1,13 +1,15 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Astar {
-    public Node as() {
+class AStar {
+    public static Node as() {
         long startTime = System.nanoTime();
 
         List<Node> map = MazeMap.curr_map;
-        ArrayList<Node> closedSet = new ArrayList<Node>();
-        ArrayList<Node> openSet = new ArratList<Node>();
+
+        List<Node> closedSet = new ArrayList<Node>();
+        List<Node> openSet = new ArrayList<Node>();
+
         Node goal = MazeMap.destination;
 
         openSet.add(MazeMap.startNode);
@@ -23,11 +25,11 @@ public class Astar {
                 if (closedSet.contains(node))
                     continue;        // Ignore the neighbor which is already evaluated.
 
-                if (openSet.contains(node) = false)    // Discover a new node
+                if (openSet.contains(node) == false)    // Discover a new node
                     openSet.add(node);
 
                 // The distance from start to a neighbor
-                int tentative_gScore :=currentNode.gScore() + currentNode.manhattanDistance(goal);
+                int tentative_gScore =currentNode.gScore() + currentNode.manhattanDistance(goal);
                 if (tentative_gScore >= node.gScore())
                     continue;    // This is not a better path.
 
@@ -40,13 +42,14 @@ public class Astar {
         long endTime = System.nanoTime();
         long deltaTime = endTime - startTime;
         System.out.println("Elapsed time (ms): " + deltaTime / 1000000 + "ms");
+        return null;
     }
 
-    public Node findMinF(ArrayList<Node> openSet) {
-        int f;
+    public static int findMinF(List<Node> openSet) {
+        int f = 0;
         int result = 0;
         for (Node node : openSet) {
-            int score = node.gScore + node.manhattanDistance();
+            int score = node.gScore + node.manhattanDistance(MazeMap.destination);
             if (score <= f || f == 0) {
                 f = score;
                 result = openSet.indexOf(node);
