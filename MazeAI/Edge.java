c833f99bd261f44212ad10c6f894141e03e095e3
everbrightw <yusencs@163.com>
2017-10-02 00:35:40 -0500

    Edge

---
+++ MazeAI/Edge.java
@@ -0,0 +1,15 @@
+
+class Edge{
+	Node beginNode;
+	Node endNode;
+	int weight;//acquired from astar getdistance()
+
+	public Edge(Node begin, Node end, int weight){
+		beginNode = begin;
+		endNode = end;
+		this.weight = weight;
+	}
+
+	
+
+}
